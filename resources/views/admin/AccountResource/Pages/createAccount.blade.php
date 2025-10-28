8183ee724b8b26d13b0f6a102ddc775a85dafd34
FerdaKoplo <sahmuraivan43@gmail.com>
2025-10-28 18:57:04 +0700

    placeholder in admin done

--- resources/views/admin/AccountResource/Pages/createAccount.blade.php
+++ resources/views/admin/AccountResource/Pages/createAccount.blade.php
@@ -11,14 +11,14 @@
                 <div>
                     <label for="name" class="block text-sm font-medium text-gray-700">Nama Lengkap</label>
                     <input type="text" id="name" name="name" required
-                        class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
+                        placeholder="Masukkan nama lengkap" class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
                 </div>
 
                 <!-- Email -->
                 <div>
                     <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="email" id="email" name="email" required
-                        class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
+                        placeholder="Masukkan email" class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
                 </div>
 
                 {{-- no Handphone --}}
@@ -26,14 +26,14 @@
                 <div>
                     <label for="no_handphone" class="block text-sm font-medium text-gray-700">No Handphone</label>
                     <input type="no_handphone" id="no_handphone" name="no_handphone" required
-                        class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
+                        placeholder="Masukkan nomor handphone" class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
                 </div>
 
                 <!-- Password -->
                 <div>
                     <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                     <input type="password" id="password" name="password" required
-                        class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
+                     placeholder="Masukkan password"   class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
                 </div>
 
                 <!-- Konfirmasi Password -->
@@ -41,7 +41,7 @@
                     <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Konfirmasi
                         Password</label>
                     <input type="password" id="password_confirmation" name="password_confirmation" required
-                        class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
+                        placeholder="Masukkan ulang password anda" class="mt-1 block w-full bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 text-gray-800 focus:ring-2 focus:ring-amber-700 focus:outline-none">
                 </div>
 
                 <!-- Aksi -->
